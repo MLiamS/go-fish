f7ca4d57045f9e2aaedce77bd235ffd544074f85
jahan walsh and liam Stabeno <liam@>
2017-03-23 16:14:21 -0700

    finished for the day.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+  private String[] suitArray = {"Spades", "Clubs", "Diamonds", "Hearts"};
+  private String[] rankArray = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
+  private ArrayList<String> mDeck = new ArrayList<String>();
+  private ArrayList<String> mPlayerOneHand = new ArrayList<String>();
+  private ArrayList<String> mPlayerTwoHand = new ArrayList<String>();
+  private String mPlayerOne;
+  private String mPlayerTwo;
+
+
+  public Game(String playerOne, String playerTwo){
+    mPlayerOneHand = returnPlayerOneDeck();
+    // mPlayerTwoHand = ;
+    mDeck = returnDeckArray();
+    // mPlayerTwoHand
+
+  }
+  public ArrayList<String> getDeck(){
+    return mDeck;
+  }
+  public ArrayList<String> getPlayerOneHand() {
+    return mPlayerOneHand;
+  }
+  public ArrayList<String> getPlayerTwoHand() {
+    return mPlayerTwoHand;
+  }
+
+  public String getPlayerOne() {
+    return mPlayerOne;
+  }
+
+  public void takeCard() {
+  String card = mDeck.remove(0);
+    mPlayerOneHand.add(card);
+
+  }
+public ArrayList<String> returnPlayerOneDeck()  {
+   ArrayList<String> x = new ArrayList<String>();
+  return x;
+}
+public ArrayList<String> returnDeckArray()  {
+  ArrayList<String> x = new ArrayList<String>();
+  for(String suit : suitArray){
+    for(String rank : rankArray){
+      x.add(rank + " of " + suit);
+    }
+  }
+  return x;
+}
+
 
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -3,4 +3,27 @@ import static org.junit.Assert.*;
 
 public class GameTest {
 
+  @Test
+  public void Game_instantiatesCorrectly_true() {
+    Game myGame = new Game("p1","p2");
+    assertEquals(true, myGame instanceof Game);
+  }
+  @Test
+  public void Deck_isEqualtoMDeck_true() {
+    Game myGame = new Game("p1","p2");
+    assertEquals(myGame.getDeck() , myGame.returnDeckArray());
+  }
+
+  @Test
+  public void Test_doesDeckRemoveCard0() {
+    Game myGame = new Game("p1","p2");
+    myGame.takeCard();
+  String expected = "ace of Spades";
+  assertEquals(expected, myGame.getPlayerOneHand().get(0));
+  }
+  // @Test
+  // public void deck_DoesDeckEqualDeckArray_true() {
+  //   Game myGame = new Game();
+  //   assertEquals(deck, myGame.returnDeckArray());
+  // }
 }
